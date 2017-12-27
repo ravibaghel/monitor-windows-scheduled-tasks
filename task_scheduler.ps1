ccaa66ef30ff0250f023d6467b41a55ce8c2e9eb
Ravi Baghel <ravibaghel@outlook.com>
2017-12-27 14:53:17 -0500

    Add files via upload

---
+++ task_scheduler.ps1
@@ -0,0 +1,20 @@
+﻿param(
+[string]$tasknametocheck = "Task to check"
+)
+$head=""
+$Date = Get-Date -Format "MM/dd/yyyy"  
+$b = "<H2>Environment $tasknametocheck status from $Date</H2>" 
+$task_info = Get-ScheduledTask -TaskName $tasknametocheck | Get-ScheduledTaskInfo
+$body = $task_info | select TaskName, LastRunTime, LastTaskResult, NextRunTime, NumberOfMissedRuns | convertto-html -head $head -body $b                 
+If ($task_info.LastTaskResult -eq 0) {       
+	$subject = "$tasknametocheck Scheduled Task Success" 
+} else {
+	$subject = "$tasknametocheck Scheduled Task Failure" 
+}
+$smtpServer = "smtp.server" 
+$mailer = new-object Net.Mail.SMTPclient($smtpserver)              
+$From = "no-reply@ravibaghel.com" 
+$To = "demo@ravibaghel.com" 
+$msg = new-object Net.Mail.MailMessage($from,$to,$subject,$body)   
+$msg.IsBodyHTML = $true 
+$mailer.send($msg)
